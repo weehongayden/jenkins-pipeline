4a7009b1db0f78e8ff34dfde332fb1e1e7579b79
Wee Hong KOH <weehongayden.developer@hotmail.com>
2023-12-29 13:40:15 +0800

    add vault automation script

--- vault/script/generate_token.sh
+++ vault/script/generate_token.sh
@@ -1,33 +1,66 @@
-#!/bin/bash
+#!/bin/sh
 set -euxo pipefail
 
-export CREDENTIAL_PATH="/home/vault/credentials"
-export VAULT_ADDR="https://0.0.0.0:8200"
+# Read environment variables from .env file
+while IFS= read -r line; do
+  # Check if the line is not a comment (does not start with #)
+  if [ "${line#"#"}" != "$line" ]; then
+    continue
+  fi
+  
+  # Export the environment variable
+  export "$line"
+done < .env
 
-export JENKINS_USERNAME="weehong"
-export JENKINS_TOKEN="11e29411fa9ee268c2431f537dd4ac6744"
-export JENKINS_URL="jenkins.weehong.dev"
+# Check the Vault status
+export VAULT_STATUS=$(vault status -format "json" | jq -r '.sealed')
 
-exec > >(tee -a log.txt) 2>&1
+if [[ $VAULT_STATUS == 'false' ]]; then
+  echo "Vault is already unsealed."
+else
+  echo "Vault is sealed. Unsealing..."
 
-# fetch and parse the role ID from Vault
-export VAULT_ROLE_ID="$(vault write -format=json -f auth/approle/role/developer/role-id | jq -r '.data.role_id')"
+  # Run unseal operation and iterate through the key values until the seal status changes to "false"
+  i=1
+  while [[ $VAULT_STATUS == 'true' ]]; do
+    eval "UNSEAL_KEY=\$VAULT_UNSEAL_KEY_$i"
+    vault operator unseal "$UNSEAL_KEY" &>/dev/null
+    export VAULT_STATUS=$(vault status -format "json" | jq --raw-output '.sealed')
+    i=$((i+1))
+  done
 
-# fetch and parse the secret ID from Vault
-export VAULT_SECRET_ID="$(vault write -format=json -f auth/approle/role/developer/secret-id | jq -r '.data.secret_id')"
+  echo "Vault is now unsealed."
+fi
 
-# generate and parse the app token
+# Fetch and parse the role ID from Vault
+export VAULT_ROLE_ID="$(vault read -format=json auth/approle/role/$VAULT_ROLE/role-id | jq -r '.data.role_id')"
+
+# Fetch and parse the secret ID from Vault
+export VAULT_SECRET_ID="$(vault write -format=json -f auth/approle/role/$VAULT_ROLE/secret-id | jq -r '.data.secret_id')"
+
+# Generate and parse the app token
 export VAULT_APP_TOKEN="$(vault write auth/approle/login -format=json role_id=$VAULT_ROLE_ID secret_id=$VAULT_SECRET_ID | jq -r '.auth.client_token')"
 
-# write the credential XML to a file, which we will upload to Jenkins later 
-cat > $CREDENTIAL_PATH/credential.xml <<EOF
-<org.jenkinsci.plugins.plaincredentials.impl.StringCredentialsImpl>
-  <scope>GLOBAL</scope>
-  <id>vault-approle-token</id>
-  <secret>$VAULT_APP_TOKEN</secret>
-</org.jenkinsci.plugins.plaincredentials.impl.StringCredentialsImpl>
-EOF
+# Send the credential to Jenkins using the Jenkins API endpoint
+RESPONSE=$(curl -s -o /dev/null -w "%{http_code}" \
+-X POST "https://$JENKINS_USERNAME:$JENKINS_TOKEN@$JENKINS_URL/manage/credentials/store/system/domain/_/credential/$JENKINS_CREDENTIAL_ID/updateSubmit" \
+--data-urlencode "json={
+    \"stapler-class\": \"com.datapipe.jenkins.vault.credentials.VaultTokenCredential\",
+    \"scope\": \"GLOBAL\",
+    \"token\": \"$VAULT_APP_TOKEN\",
+    \"\$redact\": \"token\",
+    \"namespace\": \"\",
+    \"id\": \"$JENKINS_CREDENTIAL_ID\",
+    \"description\": \"Vault+AppRole+Token\",
+    \"Submit\": \"\"
+  }"
+)
 
-# send the credential to Jenkins using the Jenkins API endpoint
-echo "Setting $JENKINS_URL vault-secret-id"
-curl -X POST -H content-type:application/xml -d @$CREDENTIAL_PATH/credential.xml "https://$JENKINS_USERNAME:$JENKINS_TOKEN@$JENKINS_URL/credentials/store/system/domain/_/credential/vault-secret-id/config.xml"
+CURRENT_DATETIME=$(date "+%Y-%m-%d %H:%M:%S")
+
+# Output the Jenkins API response
+if [ "$RESPONSE" -eq 302 ]; then
+  echo "$CURRENT_DATETIME: Update app role token successfully." >> output.log
+else
+  echo "$CURRENT_DATETIME: Failed to update the app role token." >> output.log
+fi
